b5e09274aabdd311f1eb4330042cb978981a7609
SkagitIan <67756434+SkagitIan@users.noreply.github.com>
2025-07-09 16:26:00 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -78,7 +78,7 @@ function renderResults(data) {
 
     const stopsList = route.stops.map((stop, idx) =>
       `<li class="list-group-item">
-        <span class="badge">${idx + 1}</span><span><strong>${stop.prop_id}</strong></span>
+        <span class="badge badge-success">${idx + 1}</span><span><strong>${stop.prop_id}</strong></span>
         <a href="https://maps.google.com/?q=${stop.latitude},${stop.longitude}" target="_blank">
           <small class="text-muted ms-2">${stop.address || "No address"}</small>
         </a>
